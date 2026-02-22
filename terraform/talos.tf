b817e9951be128be39e84011eb6b4c0ec281243a
Connor McKinnon <mckinnondconnor@gmail.com>
2026-02-22 13:16:12 -0500

    feat: add Talos machine config, bootstrap, and kubeconfig retrieval

---
+++ terraform/talos.tf
@@ -0,0 +1,90 @@
+provider "talos" {}
+
+# --- Machine Secrets ---
+
+resource "talos_machine_secrets" "this" {
+  talos_version = var.talos_version
+}
+
+# --- Machine Configuration ---
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = "https://${var.talos_node_ip}:6443"
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+  talos_version    = var.talos_version
+}
+
+# --- Client Configuration ---
+
+data "talos_client_configuration" "this" {
+  cluster_name         = var.cluster_name
+  client_configuration = talos_machine_secrets.this.client_configuration
+  endpoints            = [var.talos_node_ip]
+  nodes                = [var.talos_node_ip]
+}
+
+# --- Apply Configuration ---
+
+resource "talos_machine_configuration_apply" "controlplane" {
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+  node                        = var.talos_node_ip
+
+  config_patches = [
+    yamlencode({
+      machine = {
+        install = {
+          disk  = "/dev/sda"
+          image = "factory.talos.dev/installer/${var.talos_schematic_id}:${var.talos_version}"
+        }
+        network = {
+          hostname = var.vm_name
+          interfaces = [{
+            deviceSelector = {
+              driver = "virtio_net"
+            }
+            addresses = ["${var.talos_node_ip}/24"]
+            routes = [{
+              network = "0.0.0.0/0"
+              gateway = var.gateway
+            }]
+          }]
+        }
+      }
+    }),
+    yamlencode({
+      machine = {
+        kubelet = {
+          extraArgs = {
+            node-ip = var.talos_node_ip
+          }
+        }
+      }
+      cluster = {
+        allowSchedulingOnControlPlanes = true
+      }
+    }),
+  ]
+
+  depends_on = [proxmox_virtual_environment_vm.talos]
+}
+
+# --- Bootstrap ---
+
+resource "talos_machine_bootstrap" "this" {
+  node                 = var.talos_node_ip
+  client_configuration = talos_machine_secrets.this.client_configuration
+
+  depends_on = [talos_machine_configuration_apply.controlplane]
+}
+
+# --- Kubeconfig ---
+
+data "talos_cluster_kubeconfig" "this" {
+  node                 = var.talos_node_ip
+  client_configuration = talos_machine_secrets.this.client_configuration
+
+  depends_on = [talos_machine_bootstrap.this]
+}
