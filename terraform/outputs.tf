c4c2784f2dd26d4f205e92f5b7b3ac62965a6d1d
Connor McKinnon <mckinnondconnor@gmail.com>
2026-02-22 13:16:17 -0500

    feat: add Terraform outputs for vm_id, talosconfig, kubeconfig

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "vm_id" {
+  description = "Proxmox VM ID"
+  value       = proxmox_virtual_environment_vm.talos.vm_id
+}
+
+output "talosconfig" {
+  description = "Talos client configuration"
+  value       = data.talos_client_configuration.this.talos_config
+  sensitive   = true
+}
+
+output "kubeconfig" {
+  description = "Kubernetes kubeconfig"
+  value       = data.talos_cluster_kubeconfig.this.kubeconfig_raw
+  sensitive   = true
+}
